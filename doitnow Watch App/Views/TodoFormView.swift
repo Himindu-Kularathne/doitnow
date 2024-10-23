29519fbf97fff9dac0f8442b21c980e4c32a521f
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-10-23 13:11:59 +0530

    add todo form

---
+++ doitnow Watch App/Views/TodoFormView.swift
@@ -0,0 +1,8 @@
+//
+//  TodoFormView.swift
+//  doitnow Watch App
+//
+//  Created by Himindu Kularathne on 2024-10-23.
+//
+
+import Foundation
---
+++ doitnow Watch App/Views/TodoItem.swift
@@ -0,0 +1,8 @@
+//
+//  FormView.swift
+//  doitnow Watch App
+//
+//  Created by Himindu Kularathne on 2024-10-23.
+//
+
+import Foundation
